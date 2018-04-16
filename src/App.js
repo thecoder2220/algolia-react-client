67209195d70a4a9eb82aab528738da2adefa3524
claurier <fathera6@gmail.com>
2018-04-16 13:41:07 +0200

    début de la refactorisation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Content extends Component {
   render() {
     return (
       <div className="App">
@@ -18,4 +18,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Content;
